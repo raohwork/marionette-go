64eacc184a7e7ed7ec0bbaf7fa2ddff35cf52f0a
Ronmi Ren <ronmi.ren@gmail.com>
2019-02-27 00:26:10 +0800

    basic test cases

---
+++ mnclient/commander_base_test.go
@@ -0,0 +1,112 @@
+package mnclient
+
+import "testing"
+
+func (tc *cmdrTestCase) testNewSessionWith(t *testing.T) {
+	id, cap, err := tc.NewSessionWith("normal", true)
+	if err != nil {
+		t.Fatalf("unexpected error in NewSessionWith(): %s", err)
+	}
+	if id == "" {
+		t.Fatal("empty id")
+	}
+	if cap == nil {
+		t.Fatal("empty cap")
+	}
+
+	if cap.PageLoadStrategy != "normal" {
+		t.Errorf("unexpected page load strategy: %s", cap.PageLoadStrategy)
+	}
+
+	if !cap.AcceptInsecureCerts {
+		t.Errorf("unexpected insecure cert: %t", cap.AcceptInsecureCerts)
+	}
+
+	t.Logf("id: %s", id)
+	t.Logf("cap: %+v", cap)
+}
+
+func (tc *cmdrTestCase) testNavigate(t *testing.T) {
+	err := tc.Navigate("https://www.google.com")
+	if err != nil {
+		t.Fatalf("unexpected error in Navigate(): %s", err)
+	}
+}
+
+func (tc *cmdrTestCase) testGetWindowHandles(t *testing.T) {
+	h, err := tc.GetWindowHandles()
+	if err != nil {
+		t.Fatalf("unexpected error in GetWindowHandles(): %s", err)
+	}
+
+	if len(h) < 1 {
+		t.Fatal("empty handles")
+	}
+
+	t.Logf("handles: %+v", h)
+}
+
+func (tc *cmdrTestCase) testNewWindow(t *testing.T) {
+	id, typ, err := tc.NewWindow("tab", true)
+	if err != nil {
+		t.Fatalf("unexpected error in NewWindow(): %s", err)
+	}
+	if id == "" {
+		t.Error("empty id")
+	}
+	if typ != "tab" {
+		t.Errorf("unexpected type: %s", typ)
+	}
+}
+
+func (tc *cmdrTestCase) testSwitchToWindow(t *testing.T) {
+	tabs, err := tc.GetWindowHandles()
+	if err != nil {
+		t.Fatalf("cannot get all tabs: %s", err)
+	}
+
+	for _, h := range tabs {
+		if err = tc.SwitchToWindow(h); err != nil {
+			t.Errorf("cannot switch to tab#%s: %s", h, err)
+		}
+	}
+}
+
+func (tc *cmdrTestCase) testGetWindowHandle(t *testing.T) {
+	h, err := tc.GetWindowHandle()
+	if err != nil {
+		t.Fatalf("unexpected error in GetWindowHandle(): %s", err)
+	}
+	if h == "" {
+		t.Error("empty handler")
+	}
+	t.Logf("handle: %s", h)
+}
+
+func (tc *cmdrTestCase) testCloseWindow(t *testing.T) {
+	handles, err := tc.CloseWindow()
+	if err != nil {
+		t.Fatalf("unexpected error in CloseWindow(): %s", err)
+	}
+	if len(handles) < 1 {
+		t.Errorf("invalid handles returned: %+v", handles)
+	}
+
+	// clean up: switch to another window
+	t.Run("CleanUp", tc.testSwitchToWindow)
+}
+
+func (tc *cmdrTestCase) testGetCurrentURL(t *testing.T) {
+	t.Run("ensure-tabs", tc.ensureSingleTab)
+	if err := tc.Navigate("about:about"); err != nil {
+		t.Fatalf("cannot go to about:about: %s", err)
+	}
+
+	uri, err := tc.GetCurrentURL()
+	if err != nil {
+		t.Fatalf("cannot get current url: %s", err)
+	}
+	if uri != "about:about" {
+		t.Errorf("unexpected url: %s", uri)
+	}
+}
---
+++ mnclient/commander_test.go
@@ -0,0 +1,101 @@
+package mnclient
+
+import (
+	"os"
+	"testing"
+
+	"github.com/raohwork/marionette-go/mnsender"
+)
+
+var addr = "127.0.0.1:2828"
+
+func init() {
+	if x := os.Getenv("MARIONETTE_ADDR"); x != "" {
+		addr = x
+	}
+}
+
+func TestCommander(t *testing.T) {
+	sender, err := mnsender.NewTCPSender(addr, 0)
+	if err != nil {
+		t.Fatalf("unexpected error in NewTCPSender(): %s", err)
+	}
+	sender.Start()
+	defer sender.Close()
+	cl := &Commander{Sender: sender}
+
+	tc := &cmdrTestCase{Commander: cl}
+
+	t.Run("NewSessionWith", tc.testNewSessionWith)
+
+	// basic commands, need these to use tc.with()
+	t.Run("NewWindow", tc.testNewWindow)
+	t.Run("SwitchToWindow", tc.testSwitchToWindow)
+	t.Run("GetWindowHandles", tc.testGetWindowHandles)
+	t.Run("GetWindowHandle", tc.testGetWindowHandle)
+	t.Run("CloseWindow", tc.testCloseWindow)
+	t.Run("Navigate", tc.testNavigate)
+	t.Run("GetCurrentURL", tc.testGetCurrentURL)
+}
+
+type cmdrTestCase struct {
+	*Commander
+}
+
+// ensuring only one tab opened
+func (tc *cmdrTestCase) ensureSingleTab(t *testing.T) {
+	tabs, err := tc.GetWindowHandles()
+	if err != nil {
+		t.Fatalf("Setup:cannot get tab list: %s", err)
+	}
+
+	if err = tc.SwitchToWindow(tabs[0]); err != nil {
+		t.Fatalf("Setup: cannot switch to first tab: %s", err)
+	}
+	if l := len(tabs); l > 1 {
+		for l > 1 {
+			tabs, err = tc.CloseWindow()
+			l = len(tabs)
+			if err != nil {
+				t.Fatalf("Setup: cannot close window: %s", err)
+			}
+			if err = tc.SwitchToWindow(tabs[0]); err != nil {
+				t.Fatalf("Setup: cannot switch to next tab: %s", err)
+			}
+		}
+	}
+}
+
+// ensuring only one tab opened and navigates to about:blank
+func (tc *cmdrTestCase) setup(t *testing.T) {
+	t.Run("tab-count", tc.ensureSingleTab)
+	if err := tc.Navigate("about:blank"); err != nil {
+		t.Fatalf("cannot navigate to blank page: %s", err)
+	}
+}
+
+// ensuring only one tab opened and navigates to about:logo
+func (tc *cmdrTestCase) teardown(t *testing.T) {
+	t.Run("tab-count", tc.ensureSingleTab)
+	if err := tc.Navigate("about:logo"); err != nil {
+		t.Fatalf("cannot navigate to logo page: %s", err)
+	}
+}
+
+// navigate to a page
+func (tc *cmdrTestCase) nav(uri string) (ret func(*testing.T)) {
+	return func(t *testing.T) {
+		if err := tc.Navigate(uri); err != nil {
+			t.Fatalf("failed to goto %s: %s", uri, err)
+		}
+	}
+}
+
+// run selected test case with setup and teardown
+func (tc *cmdrTestCase) with(f func(*testing.T)) (ret func(*testing.T)) {
+	return func(t *testing.T) {
+		t.Run("Setup", tc.setup)
+		t.Run("Case", f)
+		t.Run("Teardown", tc.teardown)
+	}
+}
