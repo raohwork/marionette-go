59ccaa9deed009f0ecd82dadd11a98110e417e6d
Ronmi Ren <ronmi.ren@gmail.com>
2019-03-02 19:20:40 +0800

    add lgpl claimer

--- mncmd/window_op.go
+++ mncmd/window_op.go
@@ -7,8 +7,23 @@ import marionette "github.com/raohwork/marionette-go"
 // See GeckoDriver.prototype.newWindow
 // https://github.com/mozilla/gecko-dev/blob/master/testing/marionette/driver.js#L2712
 type NewWindow struct {
-	Type  string `json:"type,omitempty"` // can be tab (default) or window
-	Focus bool   `json:"focus,omitempty"`
+	Type string `json:"type// This file is part of marionette-go
+//
+// marionette-go is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// marionette-go is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License along
+// with marionette-go. If not, see <https://www.gnu.org/licenses/>.
+
+,omitempty"` // can be tab (default) or window
+	Focus bool `json:"focus,omitempty"`
 }
 
 func (c *NewWindow) Decode(msg *marionette.Message) (id, typ string, err error) {
--- tabmgr/doc.go
+++ tabmgr/doc.go
@@ -1,2 +1,17 @@
+// This file is part of marionette-go
+//
+// marionette-go is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// marionette-go is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License along
+// with marionette-go. If not, see <https://www.gnu.org/licenses/>.
+
 // Package tabmgr contains an asynchronous, tab drivern client
 package tabmgr
