9c342348da4de1b3e2cede5a7b8173c4cfbb13dd
Ronmi Ren <ronmi.ren@gmail.com>
2019-03-04 23:21:47 +0800

    do not test screenshot command with hashsum
    * Behavier of ScreenshotViewport changed in fx65
    * There might be a bug when you take screenshot in headless mode:
      default style of form-input is gone.

--- mnclient/commander_screenshot_test.go
+++ mnclient/commander_screenshot_test.go
@@ -16,9 +16,6 @@
 package mnclient
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"io/ioutil"
 	"testing"
 
 	marionette "github.com/raohwork/marionette-go"
@@ -29,12 +26,8 @@ func (tc *cmdrTestCase) testScreenshotDocument(t *testing.T) {
 	if err != nil {
 		t.Fatalf("cannot take document screenshot: %s", err)
 	}
-	sum := md5.Sum(png)
-	sumStr := hex.EncodeToString(sum[:])
-	expected := `9ac5ddf51eea2f3af318bd486a39c5b9`
-	if sumStr != expected {
-		ioutil.WriteFile("document.png", png, 0644)
-		t.Fatal("hash mismatch, dumped png to document.png")
+	if len(png) == 0 {
+		t.Fatal("empty png")
 	}
 }
 
@@ -43,12 +36,8 @@ func (tc *cmdrTestCase) testScreenshotViewport(t *testing.T) {
 	if err != nil {
 		t.Fatalf("cannot take viewport screenshot: %s", err)
 	}
-	sum := md5.Sum(png)
-	sumStr := hex.EncodeToString(sum[:])
-	expected := `6141b756eaad8fccb46659d338271863`
-	if sumStr != expected {
-		ioutil.WriteFile("viewport.png", png, 0644)
-		t.Fatal("hash mismatch, dumped png to viewport.png")
+	if len(png) == 0 {
+		t.Fatal("empty png")
 	}
 }
 
@@ -58,11 +47,7 @@ func (tc *cmdrTestCase) testScreenshotElement(t *testing.T) {
 	if err != nil {
 		t.Fatalf("cannot take element screenshot: %s", err)
 	}
-	sum := md5.Sum(png)
-	sumStr := hex.EncodeToString(sum[:])
-	expected := `b58e2f91b47370be2b0ead4f265f80ae`
-	if sumStr != expected {
-		ioutil.WriteFile("element.png", png, 0644)
-		t.Fatal("hash mismatch, dumped png to element.png")
+	if len(png) == 0 {
+		t.Fatal("empty png")
 	}
 }
